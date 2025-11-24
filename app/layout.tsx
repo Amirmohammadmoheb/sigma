b3b70588b6dba94820bc741e7925c8721ebb64a9
Amirmohammadmoheb <amirmohammdmoheb45@gmail.com>
2025-11-23 20:41:13 -0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <Script src="/appmin.js" strategy="afterInteractive"/>
-      </head>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
